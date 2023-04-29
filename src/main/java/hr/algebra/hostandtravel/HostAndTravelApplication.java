7a2cd5320628000c5c7f9127b399891a7654917a
Antonio Bajo <abajo@racunarstvo.hr>
2023-04-29 19:24:45 +0200

    project created

---
+++ src/main/java/hr/algebra/hostandtravel/HostAndTravelApplication.java
@@ -0,0 +1,13 @@
+package hr.algebra.hostandtravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostAndTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HostAndTravelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/algebra/hostandtravel/HostAndTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.algebra.hostandtravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostAndTravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
